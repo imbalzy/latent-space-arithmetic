7df21dff81fb2ad3e9810a761582160c8047b51d
imbalzy <34955962+imbalzy@users.noreply.github.com>
2020-12-04 18:09:53 +0800

    origin

--- code/preprocess.py
+++ code/preprocess.py
@@ -25,18 +25,14 @@ def load_embeddings(file_path):
 # In[18]:
 
 
-def load_images(file_path, batch_size = 32,img_height = 160,img_width = 160):
+def load_images(file_path = "/git-repos/latent-space-arithmetic/dataset/", batch_size = 128,img_height = 80,img_width = 80):
     import pathlib
     data_dir = pathlib.Path(file_path)
     image_count = len(list(data_dir.glob('*/*.jpg')))
     labels=[i+1 for i in range(image_count)]
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(data_dir, labels=labels,subset=None,seed=123,image_size=(img_height, img_width),batch_size=batch_size)
-#     val_ds = tf.keras.preprocessing.image_dataset_from_directory(data_dir, labels=labels, validation_split=0.2,subset="validation",seed=123,image_size=(img_height, img_width),batch_size=batch_size)
-    return train_ds
-# def normalize_images(ds):
-#     normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-#     normalized_ds = ds.map(lambda x, y: (normalization_layer(x), y))
-#     image_batch, labels_batch = next(iter(normalized_ds))
+    ds = tf.keras.preprocessing.image_dataset_from_directory(data_dir, labels=labels,subset=None,seed=123,image_size=(img_height, img_width),batch_size=batch_size)
+    return ds
+
 
 
 # In[32]:
@@ -62,9 +58,9 @@ def load_attributes(file_path):
 
 
 if __name__ == '__main__':
-    d = load_embeddings("/git-repos/latent-space-arithmetic/dataset/embedding/glove.6B.50d.txt")
+    d = load_embeddings("/git-repos/latent-space-arithmetic/dataset/embedding/glove.6B.100d.txt")
     print(d['o'])
-    train_ds = load_images("/git-repos/latent-space-arithmetic/dataset/")
+    train_ds = load_images(file_path = "/git-repos/latent-space-arithmetic/dataset/")
     load_attributes("/git-repos/latent-space-arithmetic/dataset/Anno/list_attr_celeba.txt")
     import matplotlib.pyplot as plt
 
